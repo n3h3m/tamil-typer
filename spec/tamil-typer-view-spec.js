0c7e8a7ace5d308f42449f4eb5563096410c9a91
nehemiah jacob <nehemiah.jacob@farifaxmedia.com.au>
2018-02-13 14:41:56 +1100

    git init

---
+++ spec/tamil-typer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TamilTyperView from '../lib/tamil-typer-view';
+
+describe('TamilTyperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
